1da4536421b27383cefc08ae465ebb84116235a8
Joao Pedro Marques <joaopedrogmarques@gmail.com>
2024-10-29 02:45:21 -0400

    implemented onboarding page, made changes to calendar and dashboard

--- app/src/main/java/com/example/bloombotanica/MainActivity.java
+++ app/src/main/java/com/example/bloombotanica/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.bloombotanica;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
