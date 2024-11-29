0b46ae2f5d297cd607bacc0b3590a2b2b00a4273
Zorah Jawadi <89284997+ZJawadi@users.noreply.github.com>
2024-11-28 21:33:06 -0500

    Added a listview for settings

--- app/src/main/java/com/example/bloombotanica/ui/SettingsFragment.java
+++ app/src/main/java/com/example/bloombotanica/ui/SettingsFragment.java
@@ -1,12 +1,20 @@
 package com.example.bloombotanica.ui;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.bloombotanica.R;
 
@@ -25,6 +33,9 @@ public class SettingsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    ListView l;
+    String settings[] = { "Theme", "Log View" };
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -61,6 +72,14 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, settings);
+        ListView lv = (ListView) rootView.findViewById(R.id.list);
+        lv.setAdapter(adapter);
+        return rootView;
     }
+
+
+
 }
